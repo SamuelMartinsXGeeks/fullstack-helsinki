b3506999c6bdccd9dcbae7be9eb42dc729ec6b2d
Samuel  Batista Martins <s.martins@kigroup.de>
2022-05-12 15:51:55 +0100

    added PUT to update notes

--- backend/src/index.js
+++ backend/src/index.js
@@ -53,6 +53,12 @@ app.get('/api/notes/:id', (request, response) => {
   response.json(note);
 });
 
+app.put('/api/notes/:id', (request, response) => {
+  const id = +request.params.id;
+  const note = notes.find(note => note.id === id);
+  response.json(note);
+});
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
